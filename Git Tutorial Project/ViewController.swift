452d5a6b8b2392f39d63d59d2d390225340cec39
johnekey <johnekey@apples-air.attlocal.net>
2019-01-23 11:11:57 -0600

    Initial Commit

---
+++ Git Tutorial Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Tutorial Project
+//
+//  Created by johnekey on 1/23/19.
+//  Copyright © 2019 johnekey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
